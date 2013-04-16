4b76f6f9edbcd3a94bd2f7fbee014803790dd24d
Alexander Rust <mail@alr.st>
2013-04-16 22:34:16 +0200

    Commented calendar code

--- components/calendar/calendar.js
+++ components/calendar/calendar.js
@@ -25,15 +25,17 @@ var MQTT_TOPIC_SYS = "/sys/"+ argv.systemId + "/#"
 var MQTT_TOPIC_CALENDAR_ID = "/sys/" + argv.systemId + "/calendarId";
 var MQTT_TOPIC_REFRESH_TOKEN = "/sys/" + argv.systemId + "/refreshToken";
 
+// Connects to the broker when the application is started
 (function connect() {
 	homa.mqttHelper.connect();
 })();
 
-
+// Subscribes to the sys topic to receive config values
 homa.mqttHelper.on('connect', function(packet) {
 	homa.mqttHelper.subscribe(MQTT_TOPIC_SYS);
 });
 
+// Called when a MQTT message is received. Calls oauth2bootstrap() when all required config parameters are received
 homa.mqttHelper.on('message', function(packet) {
 	settings[packet.topic] = packet.payload;
 	if (bootstrapComplete() && !bootstrapCompleted) {
@@ -43,6 +45,7 @@ homa.mqttHelper.on('message', function(packet) {
 	}
 });
 
+// Checks if all required config parameters are received
 function bootstrapComplete() {
 		var pass = true
 		var requiredItems = [MQTT_TOPIC_CALENDAR_ID];
@@ -55,23 +58,26 @@ function bootstrapComplete() {
 		return pass;
 }
 
+// Checks if there is a saved OAuth refresh token or if the application has to be authorized to access the user's calendar
 function oauth2bootstrap() {
 	oa = new oauth.OAuth2(clientId, clientSecret, "https://accounts.google.com/o", "/oauth2/auth", "/oauth2/token");
 	if(settings[MQTT_TOPIC_REFRESH_TOKEN] == undefined || settings[MQTT_TOPIC_REFRESH_TOKEN] == "" )
-		oauth2authorize();
+		oauth2authorize(); // No refresh token. Request autorization
 	else
-		oauth2refreshAccessToken();
+		oauth2refreshAccessToken(); // Refresh token present, Request a OAuth2 access token and start querying the calendar
 }
 
+// Requests authorization (using https://developers.google.com/accounts/docs/OAuth2ForDevices) to access the user's calendar. 
 function oauth2authorize(){
 	homa.logger.info("OAUTH2", "No refresh token provided. Requesting authorization from user.");
-
+	// Create inital request to obtain a "user code"
 	var initialRequestData = querystring.stringify({'client_id' : clientId, 'scope' : 'https://www.googleapis.com/auth/calendar'}); 
 	var initialRequestOptions = {host: 'accounts.google.com', port : 443, path: '/o/oauth2/device/code', method: 'POST', headers: { 'Content-Type': 'application/x-www-form-urlencoded', 'Content-Length': initialRequestData.length}};
 	var initialRequest = https.request(initialRequestOptions, function(response) {
 	  response.setEncoding('utf8');
 	  response.on('data', function (chunk) {
 	    	var initialResponse = JSON.parse(chunk);
+	    	// Query function that periodically checks if the user has entered the code and granted access already
 	  		var query = function() {
 					var secondaryRequestData = querystring.stringify({'client_id' : clientId, 'client_secret' : clientSecret, 'code' : initialResponse.device_code, 'grant_type' : 'http://oauth.net/grant_type/device/1.0'}); 
 					var secondaryRequestOptions = {host: 'accounts.google.com', port : 443, path: '/o/oauth2/token', method: 'POST', headers: { 'Content-Type': 'application/x-www-form-urlencoded', 'Content-Length': secondaryRequestData.length}};						
@@ -79,10 +85,10 @@ function oauth2authorize(){
 	  				response.setEncoding('utf8');
 	  				response.on('data', function (chunk) {
 	  						var secondaryResponse = JSON.parse(chunk);
-	  						if(secondaryResponse.error) {
+	  						if(secondaryResponse.error) {	// User has not yet entered the code and granted access
 	  							setTimeout(query, initialResponse.interval*1000+5000);
 									homa.logger.info("OAUTH2", "Authorization pending. Please go to %s and enter the code: %s", initialResponse.verification_url, initialResponse.user_code);
-	  						} else {
+	  						} else {	// User has entered the code to authorize this application. The response includes an initial OAuth access and refresh token.
 									oauth2parseAccessToken(secondaryResponse);
 	  						}
 						});
@@ -91,6 +97,8 @@ function oauth2authorize(){
 					secondaryRequest.write(secondaryRequestData);  
 					secondaryRequest.end();  
 	  		}
+
+	  		// Send query
 	  		query();
 	  });
 	});
@@ -100,6 +108,9 @@ function oauth2authorize(){
 	initialRequest.end();  
 }
 
+// Parses a API response that contains an access token (and possible a refresh token)
+// Schedules the refresh of the access token before it expires
+// Also initiates the scheduling of regular calendar queries 
 function oauth2parseAccessToken(results) {
   accessToken = results.access_token;
   accessTokenRefreshIn = !!results.expires_in ? (results.expires_in-600)*1000: accessTokenRefreshIn;
@@ -116,6 +127,7 @@ function oauth2parseAccessToken(results) {
  		process.nextTick(calendarScheduleQuery);
 }
 
+// Refreshes the OAuth access token with an existing refresh token
 function oauth2refreshAccessToken() {
 	homa.logger.info("OAUTH", "Refreshing access token");
   oa.getOAuthAccessToken(settings[MQTT_TOPIC_REFRESH_TOKEN], {grant_type:'refresh_token'}, function(err, access_token, refresh_token, results) {
@@ -126,11 +138,13 @@ function oauth2refreshAccessToken() {
 		});
 }
 
+// Schedules regular calendar queries 
 function calendarScheduleQuery(){
 	setTimeout(calendarQuery, calendarQueryInterval);
 	process.nextTick(calendarQuery);
 }
 
+// Queries the calendar API and schedules publishes depending on the returned events that start during the specified query intervall
 function calendarQuery() {
 	var timeMax = encodeURIComponent(new Date((new Date()).getTime()+ (calendarQueryInterval + (5*60*1000))).toISOString());
 	var query = "https://www.googleapis.com/calendar/v3/calendars/"+settings[MQTT_TOPIC_CALENDAR_ID]+"/events?singleEvents=true&fields=items(id%2Cdescription%2Cstart%2Cend%2Csummary)&orderBy=startTime&timeMin="+encodeURIComponent(new Date().toISOString())+"&timeMax="+timeMax;	
