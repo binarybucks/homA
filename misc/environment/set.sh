26e91612391a167144da6a9f866d9bb1ed515463
Alexander Rust <mail@alr.st>
2013-04-16 12:54:34 +0200

    modified vars

--- misc/environment/set.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Exports key=value pairs in vars files as environment variables
-# Check conents of vars file prior to execution
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" 
-while read line; do 
-	export $line; 
-done < "$DIR/vars"
-
