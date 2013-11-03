d4711bb8a536d4d4b9867be37b3f350e4cb5e4f8
Alexander Rust <mail@alr.st>
2013-11-03 11:01:32 +0100

    closes #127

--- tools/publish/publish.js
+++ tools/publish/publish.js
@@ -3,9 +3,10 @@ var homa = require('homa');
 homa.params.describe("topic", "The topic to which the payload will be published")
 										.describe("payload", "The payload that will be published to the topic. If no payload is specified an empty string will be published to clear retained messages")
 										.describe("retained", "Whether to set the retained flag on the message that will be published")
-										.demand("topic")
-		   							.default("payload", '')
+		   							.demand("topic")
+		   							.default("payload", '')		   					
 										.alias("topic", "t")
+										.default("clear", false)
 										.alias("payload", "p")
 										.alias("retained", "r")
 										.default("retained", false);
@@ -13,7 +14,11 @@ var systemId = homa.paramsWithDefaultSystemId("482924-publish");
 
 homa.mqttHelper.on('connect', function() {
 	console.log(typeof homa.params.argv.payload)
-	homa.mqttHelper.publish(homa.params.argv.topic, homa.params.argv.payload, homa.params.argv.retained);
+	if(homa.params.argv.topic != '')
+		homa.mqttHelper.publish(homa.params.argv.topic, homa.params.argv.payload, homa.params.argv.retained);
+	else
+		homa.mqttHelper.publish(homa.params.argv.topic, "", true);
+
 	homa.mqttHelper.disconnect();
 });
 
