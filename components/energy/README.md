5fc963bd03e57647f769e92da35431f9b5462c08
Holger Mueller <github@euhm.de>
2017-03-03 21:17:42 +0100

    shows the current power and total energy

---
+++ components/energy/README.md
@@ -0,0 +1,38 @@
+# HomA - Energy
+This component shows the current energy and active power read by an IR connector (e.g. IR-Kopf by [volkszaehler.org](http://wiki.volkszaehler.org/)) in the framework's interfaces.
+
+
+### Installation
+Install the required dependencies
+```none
+$ git clone https://github.com/hmueller01/libsml.git
+$ make
+$ sudo ln -s $HOMA_BASEDIR/components/energy/libsml.git/examples/sml_server /usr/local/bin/sml_server
+```
+Create a ```mqtt_config.py``` file with this content:
+```none
+host = "localhost"
+port = 1883
+user = ""
+pwd = ""
+```
+Modify ```energy``` sml_server device ```/dev/vzir0``` to your needs.
+
+### Usage
+Start the application manually 
+```none
+$ ./energy
+```
+
+### Systemd
+If your system supports it, you can start the application as a daemon from systemd by using the provided template.
+```none
+$ sudo ln -s $HOMA_BASEDIR/misc/systemd/homa@.service /etc/systemd/system/multi-user.target.wants/homa@energy.service
+$ sudo systemctl --system daemon-reload
+$ sudo systemctl start homa@energy.service
+```
+
+Logs are then availble in the systemd journal 
+```
+$ sudo journalctl -u homa@energy.service -n 100 -f
+```
---
+++ components/energy/sml_mqtt.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python2
+# $Id$
+# Reads SML data created by sml_server from stdin and sends it to a 
+# MQTT broker used by HomA framework.
+# Holger Mueller
+# 2017/03/02
+
+import sys
+import os.path
+import paho.mqtt.client as mqtt
+import mqtt_config
+
+# config here ...
+debug = False
+systemId = "123456-energy"
+device = "Energy"
+room = "Home"
+# Reminder if HomA setup messages have been send, delete and restart to resend
+init_file = "/dev/shm/homa_init."+ systemId
+
+
+obis_arr = [
+	{'obis': '1-0:1.8.0*255', 'scale': 1000, 'unit': ' kWh', 'topic': 'total', 'name': 'Total Energy'},
+	{'obis': '1-0:16.7.0*255', 'scale': 1, 'unit': ' W', 'topic': 'current', 'name': 'Current Power'}]
+
+def get_topic(t1 = None, t2 = None, t3 = None):
+	"Create topic string."
+	topic = "/devices/"+ systemId
+	if t1:
+		topic += "/"+ t1
+	if t2:
+		topic += "/"+ t2
+	if t3:
+		topic += "/"+ t3
+	if debug: print("topic string = '"+ topic+ "'")
+	return topic
+
+def scan_line(line):
+	"Scan input line and if found publish MQTT topic/value. Returns True if success, otherwise False."
+	for obis_dict in obis_arr:
+		if line.find(obis_dict['obis']) <> -1:
+			# find value between "#"
+			pos_start = line.find("#") + 1
+			pos_end = line.find("#", pos_start)
+			value = float(line[pos_start:pos_end])
+			if debug: print("Found obis topic '"+ obis_dict['topic']+ "' = line["+ str(pos_start)+ ":"+ str(pos_end)+ "] = '"+ str(value)+ "'")
+			# scale value as specified
+			value = round(value / obis_dict['scale'], 1)
+			mqttc.publish(get_topic("controls", obis_dict['topic']), str(value)+ obis_dict['unit'], retain=True)
+			return True
+	return False
+
+def homa_init():
+	"Publish HomA setup messages to MQTT broker."
+	print("Publishing HomA setup data ...")
+	# set room name
+	mqttc.publish(get_topic("meta/room"), room, retain=True)
+	# set device name
+	mqttc.publish(get_topic("meta/name"), device, retain=True)
+	# setup controls
+	for obis_dict in obis_arr:
+		mqttc.publish(get_topic("controls", obis_dict['topic'], "meta/type"), "text", retain=True)
+		#mqttc.publish(get_topic("controls", obis_dict['topic'], "meta/name"), obis_dict['name'], retain=True)
+	# create init file
+	file = open(init_file, 'w')
+	#file.write(data)
+	file.close()
+
+# The callback for when the client receives a CONNACK response from the broker.
+def on_connect(client, userdata, flags, rc):
+	if debug: print("Connected with result code "+ str(rc))
+
+	# Subscribing in on_connect() means that if we lose the connection and
+	# reconnect then subscriptions will be renewed.
+	#client.subscribe("$SYS/#")
+	return
+
+# The callback for when a PUBLISH message is received from the broker.
+def on_message(client, userdata, msg):
+	if debug: print(msg.topic+ " "+ str(msg.payload))
+	return
+
+# The callback for when a message is published to the broker.
+def on_publish(client, userdata, mid):
+	if debug: print("message send "+ str(mid))
+	return
+
+
+# connect to MQTT broker
+mqttc = mqtt.Client()
+mqttc.on_connect = on_connect
+mqttc.on_message = on_message
+mqttc.on_publish = on_publish
+#mqttc.tls_set(ca_certs, certfile=None, keyfile=None, cert_reqs=ssl.CERT_REQUIRED, tls_version=ssl.PROTOCOL_TLSv1, ciphers=None)
+mqttc.username_pw_set(mqtt_config.user, password=mqtt_config.pwd)
+mqttc.connect(mqtt_config.host, port=mqtt_config.port)
+mqttc.loop_start()
+
+# check if we need to init HomA
+if not os.path.isfile(init_file):
+	# setup MQTT device and control settings
+	homa_init()
+
+# read from stdin and scan for obis data
+print("Reading sml obis data from stdin forever. Press Ctrl-C to break.")
+#for line in sys.stdin: # does not work with piping, because reads to eof!
+while True:
+	try:
+		line = sys.stdin.readline()
+		if debug: print(line)
+		scan_line(line)
+	except (KeyboardInterrupt, SystemExit):
+		print '\nKeyboardInterrupt found! Stopping program.'
+		break
+
+# wait until all queued topics are published
+print 'Flushing MQTT queue ...'
+mqttc.loop_stop()
