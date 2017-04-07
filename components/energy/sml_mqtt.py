791b77cde0d26344885aead09193f58051a882f1
Holger Mueller <github@euhm.de>
2017-04-07 20:08:47 +0200

    corrected comment

--- components/energy/sml_mqtt.py
+++ components/energy/sml_mqtt.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python2
-# $Id$
 # Reads SML data created by sml_server from stdin and sends it to a 
 # MQTT broker used by HomA framework.
 # Holger Mueller
 # 2017/03/02, 2017/03/09, 2017/04/04
-# 20017/04/06 added meta/unit topic and removed unit from payload of actual value
+# 2017/04/06 added meta/unit topic and removed unit from payload of actual value
 
 import sys
 import os.path
